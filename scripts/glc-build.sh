0e17f3604700df496a03b49cfb8fa2f1cc660a13
sfan5 <sfan5@live.de>
2015-06-06 09:24:37 +0200

    Improve scripts/glc-build.sh

--- scripts/glc-build.sh
+++ scripts/glc-build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # glc-build.sh -- glc build and install script
 # Copyright (C) 2007-2008 Pyry Haulos
 #
+
+STABLE_GLC_VER=0.5.8
 
 info () {
 	echo -e "\033[32minfo\033[0m  : $1"
@@ -25,33 +27,10 @@ die () {
 	exit 1
 }
 
-download () {
-	[ -f "$1.tar.gz" ] && rm -f "$1.tar.gz"
-	wget -q "http://nullkey.ath.cx/$1/$1.tar.gz" || die "Can't fetch $1"
-}
-
-unpack () {
-	[ -d "$1" ] && rm -rdf "$1"
-	tar -xzf "$1.tar.gz" || die "Can't unpack $1.tar.gz"
-}
-
-gitfetch () {
-	GIT="`which git 2> /dev/null`"
-
-	if [ -d "$1" ]; then
-		cd "$1"
-		$GIT pull origin || die "Can't update $1"
-		cd ..
-	else
-		$GIT clone "git://github.com/nullkey/$1.git" \
-			|| die "Can't clone $1"
-	fi
-}
-
 info "Welcome to glc install script!"
 
 BUILD64=0
-uname -a | grep x86_64 > /dev/null && BUILD64=1
+[ $(uname -m) == "x86_64" ] && BUILD64=1
 
 echo "#include <stdio.h>
 	int main(int argc, char argv[]){printf(\"test\");return 0;}" | \
@@ -68,7 +47,9 @@ echo "#include <stdio.h>
 [ -e "/usr/include/png.h" ] \
 	|| die "Missing libpng headers (Ubuntu users: sudo apt-get install libpng12-dev)"
 [ -x "/usr/bin/cmake" ] \
-	|| die "CMake not installed (Ubuntu users: sudo apt-get install cmake)"
+	|| die "CMake not found (Ubuntu users: sudo apt-get install cmake)"
+[ -x "/usr/bin/git" ] \
+	|| die "git not found (Ubuntu users: sudo apt-get install git-core)"
 
 if [ $BUILD64 == 1 ]; then
 	echo "#include <stdio.h>
@@ -77,8 +58,7 @@ if [ $BUILD64 == 1 ]; then
 		|| die "Can't compile 32-bit code (Ubuntu users: sudo apt-get install gcc-multilib)"
 fi
 
-DEFAULT_CFLAGS="-O2 -msse -mmmx -fomit-frame-pointer"
-[ $BUILD64 == 0 ] && DEFAULT_CFLAGS="${DEFAULT_CFLAGS} -mtune=pentium3"
+DEFAULT_CFLAGS="-O2 -fomit-frame-pointer -mtune=generic"
 
 ask "Enter path where glc will be installed."
 ask "  (leave blank to install to root directory)"
@@ -107,155 +87,73 @@ read CFLAGS
 [ "${CFLAGS}" == "" ] && CFLAGS="${DEFAULT_CFLAGS}"
 
 USE_GIT="n"
-ask "Use git (y/n)"
-ask "  (git contains latest unstable development version)"
+ask "Use latest unstable development version (y/n)"
 ask-prompt
 read USE_GIT
 
-if [ "${USE_GIT}" == "y" ]; then
-	if [ -x "`which git 2> /dev/null`" ]; then
-		gitfetch glc
-		gitfetch glc-support
-		gitfetch elfhacks
-		gitfetch packetstream
-		cd glc && ln -sf ../glc-support ./support && cd ..
-	else
-		die "git not found (Ubuntu users: sudo apt-get install git-core)"
-	fi
+if [ -d glc ]; then
+	info "Updating sources..."
+	cd glc
+	git pull origin
+	cd ..
 else
 	info "Fetching sources..."
-	download elfhacks
-	download packetstream
-	download glc
-	
-	info "Unpacking sources..."
-	unpack elfhacks
-	unpack packetstream
-	unpack glc
+	git clone https://github.com/sfan5/glc.git glc
+fi
+
+cd glc
+git submodule init
+git submodule update
+cd ..
+
+if [ "${USE_GIT}" == "y" ]; then
+	cd . # Do nothing
+else
+	cd glc
+	git checkout ${STABLE_GLC_VER}
+	cd ..
 fi
 
 MLIBDIR="lib"
 [ $BUILD64 == 1 ] && MLIBDIR="lib64"
 
-info "Building elfhacks..."
-[ -d elfhacks/build ] || mkdir elfhacks/build
-cd elfhacks/build
-cmake .. \
-	-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
-	-DCMAKE_BUILD_TYPE:STRING="Release" \
-	-DCMAKE_C_FLAGS_RELEASE_RELEASE:STRING="${CFLAGS}" \
-	-DMLIBDIR="${MLIBDIR}" > /dev/null \
-	|| die "Can't compile elfhacks (cmake failed)"
-make > /dev/null || die "Can't compile elfhacks"
-if [ $BUILD64 == 1 ]; then
-	cd ..
-	[ -d build32 ] || mkdir build32
-	cd build32
-	cmake .. \
-		-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
-		-DCMAKE_BUILD_TYPE:STRING="Release" \
-		-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS} -m32" \
-		-DMLIBDIR="lib32" > /dev/null \
-		|| die "Can't compile 32-bit elfhacks (cmake failed)"
-	make > /dev/null || die "Can't compile 32-bit elfhacks"
-fi
-cd ../..
-
-info "Building packetstream..."
-[ -d packetstream/build ] || mkdir packetstream/build
-cd packetstream/build
-cmake .. \
-	-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
-	-DCMAKE_BUILD_TYPE:STRING="Release" \
-	-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS}" \
-	-DMLIBDIR="${MLIBDIR}" > /dev/null \
-	|| die "Can't compile packetstream (cmake failed)"
-make > /dev/null || die "Can't compile packetstream"
-if [ $BUILD64 == 1 ]; then
-	cd ..
-	[ -d build32 ] || mkdir build32
-	cd build32
-	cmake .. \
-		-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
-		-DCMAKE_BUILD_TYPE:STRING="Release" \
-		-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS} -m32" \
-		-DMLIBDIR="lib32" > /dev/null \
-		|| die "Can't compile 32-bit packetstream (cmake failed)"
-	make > /dev/null || die "Can't compile 32-bit packetstream"
-fi
-cd ../..
-
 info "Building glc..."
 
-export CMAKE_INCLUDE_PATH="${PWD}/elfhacks/src:${PWD}/packetstream/src"
-export CMAKE_LIBRARY_PATH="${PWD}/elfhacks/build/src:${PWD}/packetstream/build/src"
-
-[ -d glc/build ] || mkdir glc/build
+[ -d glc/build ] && rm -R glc/build
+mkdir glc/build
 cd glc/build
 
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS}" \
-	-DMLIBDIR="${MLIBDIR}" > /dev/null \
-	 || die "Can't compile glc (cmake failed)"
-make > /dev/null || die "Can't compile glc"
+	-DMLIBDIR="${MLIBDIR}"
+make
+
 if [ $BUILD64 == 1 ]; then
-	cd ../..
-	export CMAKE_LIBRARY_PATH="${PWD}/elfhacks/build32/src:${PWD}/packetstream/build32/src"
-	[ -d glc/build32 ] || mkdir glc/build32
-	cd glc/build32
+	cd ..
+	[ -d build32 ] && rm -R build32
+	mkdir build32
+	cd build32
 
 	cmake .. \
 		-DCMAKE_INSTALL_PREFIX:PATH="${DESTDIR}" \
 		-DCMAKE_BUILD_TYPE:STRING="Release" \
 		-DCMAKE_C_FLAGS_RELEASE:STRING="${CFLAGS} -m32" \
 		-DBINARIES:BOOL=OFF \
-		-DMLIBDIR="lib32" > /dev/null \
-		|| die "Can't compile 32-bit glc (cmake failed)"
-	make > /dev/null || die "Can't compile 32-bit glc"
-fi
-cd ../..
-
-info "Installing elfhacks..."
-cd elfhacks/build
-if [ $BUILD64 == 1 ]; then
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 64-bit elfhacks"
-	cd ../build32
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 32-bit elfhacks"
-else
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install elfhacks"
-fi
-cd ../..
-
-info "Installing packetstream..."
-cd packetstream/build
-if [ $BUILD64 == 1 ]; then
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 64-bit packetstream"
-	cd ../build32
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 32-bit packetstream"
-else
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install packetstream"
+		-DMLIBDIR="lib32"
+	make
 fi
 cd ../..
 
 info "Installing glc..."
 cd glc/build
 if [ $BUILD64 == 1 ]; then
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 64-bit glc"
+	$SUDOMAKE install
 	cd ../build32
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install 32-bit glc"
+	$SUDOMAKE install
 else
-	$SUDOMAKE install > /dev/null \
-		|| die "Can't install glc"
+	$SUDOMAKE install
 fi
 cd ../..
 
