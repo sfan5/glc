db2c78d2fab0cd26b962ca3a417b8f6cb0255b6a
sfan5 <sfan5@live.de>
2015-06-06 09:22:14 +0200

    Use mpv in scripts/play.sh

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #
-# play.sh -- playing glc stream with mplayer
+# play.sh -- playing glc stream with mpv
 # Copyright (C) 2007 Pyry Haulos
 # For conditions of distribution and use, see copyright notice in glc.h
 
-CTX="1"
+VIDEO="1"
 AUDIO="1"
 
-AUDIOFIFO="/tmp/glc-audio.fifo"
-VIDEOFIFO="/tmp/glc-video.fifo"
+AUDIOFIFO="/tmp/glc-audio$$.fifo"
+VIDEOFIFO="/tmp/glc-video$$.fifo"
 
 if [ "$1" == "" ]; then
-	echo "$0 FILE [ctx] [audio]"
+	echo "Usage: $0 FILE [video ID] [audio ID]"
 	exit 1
 fi
 
-[ "$2" != "" ] && CTX=$2
+[ "$2" != "" ] && VIDEO=$2
 [ "$3" != "" ] && AUDIO=$3
 
 mkfifo "${AUDIOFIFO}"
 mkfifo "${VIDEOFIFO}"
 
 glc-play "$1" -o "${AUDIOFIFO}" -a "${AUDIO}" &
-glc-play "$1" -o "${VIDEOFIFO}" -y "${CTX}" &
+glc-play "$1" -o "${VIDEOFIFO}" -y "${VIDEO}" &
 
-mplayer -audio-demuxer lavf -demuxer y4m -audiofile "${AUDIOFIFO}" "${VIDEOFIFO}"
+mpv --audio-file "${AUDIOFIFO}" "${VIDEOFIFO}"
 
 rm -f "${AUDIOFIFO}" "${VIDEOFIFO}"
